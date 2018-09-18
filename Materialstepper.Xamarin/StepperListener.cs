674fc45644d998211bd9760a3e76b9b737717881
azeem <azeem@spica>
2018-09-18 11:18:42 +0400

    binding errors fix

---
+++ Materialstepper.Xamarin/StepperListener.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Android.Views;
+using Com.Stepstone.Stepper;
+
+namespace Materialstepper.Xamarin
+{
+	partial class StepperListener : Java.Lang.Object, StepperLayout.IStepperListener
+	{
+		public void OnCompleted(View completeButton)
+		{
+			
+		}
+
+		public void OnError(VerificationError verificationError)
+		{
+			
+		}
+
+		public void OnReturn()
+		{
+			
+		}
+
+		public void OnStepSelected(int newStepPosition)
+		{
+			
+		}
+	}
+}
---
+++ Materialstepper.Xamarin/TabItemListner.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Materialstepper.Xamarin
+{
+	partial class TabItemListner : Java.Lang.Object, Com.Stepstone.Stepper.Internal.Widget.TabsContainer.ITabItemListener
+	{
+		public void OnTabClicked(int position)
+		{
+			
+		}
+	}
+}
